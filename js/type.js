0bd8811ffe5ec97c718040a6ff6f756d9b76ffeb
zengdixiansheng <52190186+zengdixiansheng@users.noreply.github.com>
2025-10-01 01:24:05 +0800

    Update type.js

--- js/type.js
+++ js/type.js
@@ -3,5 +3,6 @@ const types = [
   { id: 'music', name: '🎵 音乐', file: 'js/data/music.js', optional: false },
   { id: 'video', name: '🎬 视频', file: 'js/data/video.js', optional: false },
   { id: 'test1', name: '🎮 test1', file: 'js/data/test1.js', optional: false },
-  { id: 'test2', name: '🎮 test2', file: 'js/data/test2.js', optional: false }
+  { id: 'test2', name: '🎮 test2', file: 'js/data/test2.js', optional: false },
+  { id: 'test3', name: '🎮 test3', file: 'js/data/test3.js', optional: false }
 ];
