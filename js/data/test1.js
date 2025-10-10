1d66018a98df2c5e84d987d4a38d3343bc78509a
zengdixiansheng <52190186+zengdixiansheng@users.noreply.github.com>
2025-10-10 15:28:31 +0800

    Update test1.js

--- js/data/test1.js
+++ js/data/test1.js
@@ -1,10 +1,10 @@
 ;(()=>{
   const links = [
     {
-      title: "Bilibili",
-      url: "https://www.bilibili.com/",
-      description: "",
-      summary: "",
+      title: "TTS",
+      url: "http://new.text-to-speech.cn/tts/",
+      description: "5ang 5ang.www",
+      summary: "1375*****2@qq.com",
       cover: "res/cover/cover.webp"
     },
     {
